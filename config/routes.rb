76b84e9d0313ba5f174ad6e4d83302948a48950e
TakatoiGo <takatoigo@TakatoiGos-MacBook-Pro.local>
2017-05-15 20:57:14 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+   resources :posts
+   devise_for:users
+   root 'post#new'
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.ht    ml
+   resources :users, only: [:index] do 
+   member do
+   get "make_friendship", to: "users#make_friendship"
+   get "accept_friendship", to: "users#accept_friendship"
+  get "decline_friendship", to: "users#decline_friendship" 
+  delete "remove_friendship", to: "users#remove_friendship"
+  end
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
