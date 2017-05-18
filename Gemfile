736124a10a6637c1fcc3cb739034906a0e6b663a
TakatoiGo <takatoigo@TakatoiGos-MacBook-Pro.local>
2017-05-18 20:00:42 +0900

    cannot sign out

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
-
+gem "therubyracer"
+gem "less-rails"
+gem "twitter-bootstrap-rails"
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,44 @@ class UsersController < ApplicationController
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
-end 
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+  def make_friendship
+    current_user.friend_request(@user) 
+ end
+
+def accept_friendship
+    @user.accept_request(current_user)
 end
+
+def decline_friendship
+  @user.decline_request(current_user) 
+end
+
+def remove_friendship
+  @user.remove_friend(current_user)
+end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.fetch(:user, {})
+    end
 end
